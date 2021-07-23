b2c31fe51b6c31ff5565de12c849718d4aaf101b
Rounak Singh <rounaksingh9431738764@gmail.com>
2021-07-23 15:35:04 +0530

    Today started the completion of new hulu web page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 const modal = document.querySelector(".modal");
 const loginBtn = document.querySelector(".login-btn");
 const closeBtn = document.querySelector(".close");
+const SectionTitle__container__down = document.querySelector(
+  ".SectionTitle__container__down-arrow"
+);
 
+SectionTitle__container__down.addEventListener("click", goto);
 loginBtn.addEventListener("click", openModal);
 closeBtn.addEventListener("click", closeModal);
 window.addEventListener("click", outsideClick);
@@ -18,4 +22,8 @@ function outsideClick() {
   if (e.target == modal) {
     closeModal();
   }
+}
+
+function goto() {
+  document.getElementById("live-sports").scrollIntoView();
 }
